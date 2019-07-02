c5493bf376e1e1e274c5b3a55810201317708a62
Scovia Nnamiggo <2305senk@gmail.com>
2019-07-02 07:53:29 +0300

    editing index page

--- index.php
+++ index.php
@@ -4,6 +4,24 @@
 	<title>points accrual</title>
 </head>
 <body>
-
+<div id="top header">
+	<img src="images/logo.jpg">
+	<h1> FENIX POINTS ACCRUAL SYSTEM </h1>
+</div>
+<div id="welcome">
+<!-- 	<form>
+		<label>Welcome</label>
+		<input type="" name="">
+	</form> -->
+	<h2 id="user">Welcome User</h2>
+</div>
+<div>
+	<h2>Please Login</h2>
+	<form>
+		<label>User name: </label><input type="text" maxlength="40" name="name"><br>
+		<label>Password: </label> <input type="Password" max="40" name="Password"><br>
+		<input type="button" name="Login">
+	</form>
+</div>
 </body>
 </html>
