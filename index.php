d96cdb2c5a2345e94ccf51d0deeebe3caaf4d345
Scovia Nnamiggo <2305senk@gmail.com>
2019-07-03 06:22:37 +0300

    creating includes for all pages

--- index.php
+++ index.php
@@ -2,26 +2,29 @@
 <html>
 <head>
 	<title>points accrual</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
-<div id="top header">
-	<img src="images/logo.jpg">
-	<h1> FENIX POINTS ACCRUAL SYSTEM </h1>
-</div>
-<div id="welcome">
-<!-- 	<form>
-		<label>Welcome</label>
-		<input type="" name="">
-	</form> -->
-	<h2 id="user">Welcome User</h2>
-</div>
-<div>
-	<h2>Please Login</h2>
-	<form>
-		<label>User name: </label><input type="text" maxlength="40" name="name"><br>
-		<label>Password: </label> <input type="Password" max="40" name="Password"><br>
-		<input type="button" name="Login">
-	</form>
-</div>
+	<div id="page_container">
+		<?PHP include "header.html"; ?>
+		<div id="welcome">
+		<!-- 	<form>
+				<label>Welcome</label>
+				<input type="" name="">
+			</form> -->
+			<h2 id="user">Welcome</h2>
+		</div>
+		<div id="main_section">
+			<h2>Please Login</h2>
+			<form>
+				<label>User name: </label><input type="text" maxlength="40" name="name"><br><br>
+				<label>Password: </label> <input type="Password" maxlength="25" name="Password"><br><br>
+				<input type="button" id="login" name="Login" value="Login"><br>
+			</form>
+		</div>
+		<?PHP include "footer.html"; ?>
+
+
+	</div>
 </body>
 </html>
