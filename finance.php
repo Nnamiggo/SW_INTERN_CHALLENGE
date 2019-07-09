81c4d5bdf65dcebcd426ac9fecb7733c0898b7e7
Scovia Nnamiggo <2305senk@gmail.com>
2019-07-09 11:43:06 +0300

    debugging

---
+++ finance.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>points accrual</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<div id="page_container">
+		<?PHP include "header.html"; ?>
+		<div id="welcome"> 
+
+			<h2 id="user">Welcome</h2> <span id="name">xx</span>
+		</div>
+		<div id="main_section">
+			<form>
+				display employeses with requests to spent points
+			
+			</form>
+
+		</div>
+		<?PHP include "footer.html"; ?>
+
+
+	</div>
+</body>
+</html>
